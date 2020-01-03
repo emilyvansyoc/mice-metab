5f613310ac44483fe3cf3e16996ba93678ca79ce
Emily Bean <epb5360@A8-AN-EVK5387-1.local>
2020-01-03 12:04:45 -0500

    fixed wrangling issue

--- R/lipidWrangling.R
+++ R/lipidWrangling.R
@@ -8,8 +8,9 @@ sampkey <- read.csv("https://raw.githubusercontent.com/EmilyB17/mice-metab/maste
 mousekey <- read.table("https://raw.githubusercontent.com/EmilyB17/mice-metab/master/data/mouseKeys.txt", header = TRUE, stringsAsFactors = FALSE)
 
 
-## wrangle the heck out of this
+## wrangle to add the proper identifying variables
 
+# add mouse ID, exercise and weight IDs
 for(i in 1:nrow(mousekey)) {
   mousekey[i, "id"] <- strsplit(mousekey$mouseID[i], "-")[[1]][3]
   mousekey[i, "Exercise"] <- strsplit(mousekey$treatmentID[i], "_")[[1]][1]
@@ -18,24 +19,30 @@ for(i in 1:nrow(mousekey)) {
 
 for(i in 1:nrow(sampkey)) {
   
-  # not all mouseIDs have length 2
+  # if the SampleName has "_" in it, it is a plasma sample (ex D35_M1)
   if(grepl("_", sampkey$SampleName[i]) == TRUE) {
     
-    # these are tumor samples; kill 2 birds w/1 stone
+    # separate out the mouse ID
     sampkey[i, "id"] <- strsplit(sampkey$SampleName[i], "_")[[1]][2]
+    sampkey[i, "tissue.type"] <- "plasma"
+   
+    # if the SampleName is only one variable, it is a tumor sample (ex M7)
+  } else {
+    
+    sampkey[i, "id"] <- sampkey$SampleName[i]
     sampkey[i, "tissue.type"] <- "tumor"
     
-  } else {
-    sampkey[i, "id"] <- sampkey$SampleName[i]
-    sampkey[i, "tissue.type"] <- "plasma"
   }
   
-  # separate positive and negative
+  # key out positive and negative
   if(grepl("POS", sampkey$SampleID[i]) == TRUE) {
     
     sampkey[i, "ion"] <- "POS"
+    
   } else {
+    
     sampkey[i, "ion"] <- "NEG"
+    
   }
   
   # for all samples: remove .wiff to identify w/metaboanalyst CSV
@@ -51,38 +58,45 @@ keys <- merge(sampkey, mousekey, by = "id")
 toSave <- keys %>% 
   select(id, shortID, tissue.type, ion, treatmentID, Exercise, Weight)
 
-##write.table(keys, "./data/allLipidKeys.txt", row.names = FALSE)
+write.table(keys, "./data/allLipidKeys.txt", row.names = FALSE)
 
 ## Finish cleaning pos and neg data
 
 # read without row1 because that is MetaboAnalyst "grouping" variable
-pos <- read.csv("https://raw.githubusercontent.com/EmilyB17/mice-metab/master/data/data_normalizedPOS.csv")[-1,]
-neg <- read.csv("https://raw.githubusercontent.com/EmilyB17/mice-metab/master/data/data_normalizedNEG.csv")[-1,]
+pos <- read.csv("https://raw.githubusercontent.com/EmilyB17/mice-metab/master/data/data_normalizedPOS.csv", stringsAsFactors = FALSE)[-1,]
+neg <- read.csv("https://raw.githubusercontent.com/EmilyB17/mice-metab/master/data/data_normalizedNEG.csv", stringsAsFactors = FALSE)[-1,]
 
 ##key <- read.table("allLipidKeys.txt", header = TRUE)
 
+# make the lipid spreadsheets vertical for merging
+
+# collect the column names minus the first one ("X", lipid identifier)
+ids <- colnames(pos)[-1]
+
 posv <- pos %>% 
-  gather(key = longID, value = "area", 2:ncol(pos)) %>% 
+  gather(key = longID, value = "log.area", ids) %>% 
   rename(lipid = X)
 # add shortID column for grouping variable to merge
 for(i in 1:nrow(posv)) {
-  posv$shortID[i] <- strsplit(posv$longID[i], ".D")[[1]][1]
+  posv$shortID[i] <- strsplit(posv$longID[i], "\\.")[[1]][1]
+  posv$SampleName[i] <- strsplit(posv$longID[i], "\\.")[[1]][2]
 }
 
-
+negids <- colnames(neg)[-1]
 negv <- neg %>% 
-  gather(key = longID, value = "area", 2:ncol(neg)) %>% 
+  gather(key = longID, value = "log.area", negids) %>% 
   rename(lipid = X)
 # add shortID column for grouping variable to merge
 for(i in 1:nrow(negv)) {
-  negv$shortID[i] <- strsplit(negv$longID[i], ".D")[[1]][1]
+  negv$shortID[i] <- strsplit(negv$longID[i], "\\.")[[1]][1]
+  negv$SampleName[i] <- strsplit(negv$longID[i], "\\.")[[1]][2]
 }
 
 # merge all with the key
 all <- rbind(negv, posv) %>% 
-  merge(keys, by = "shortID") %>% 
+  merge(keys, by = c("SampleName", "shortID"))  %>% 
   # have lots of columns; don't need to keep all
-  select(lipid, SampleName, tissue.type, ion, mouseID, treatmentID, Exercise, Weight, area)
+  select(lipid, SampleName, tissue.type, ion, mouseID, treatmentID, Exercise, Weight, log.area)
 
 # write this to file
-##write.table(all, "./data/allLipidsCleaned.txt", row.names = FALSE)
+write.table(all, "./data/allLipidsCleaned.txt", row.names = FALSE)
